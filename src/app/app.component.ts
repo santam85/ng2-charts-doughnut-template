3263f0a444018140a3193aa66d5c0c2914c30e1a
Marco Santarelli <marco@backbase.com>
2021-08-09 10:58:11 +0200

    Updated to rc.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartType } from 'chart.js';
-import { MultiDataSet, Label } from 'ng2-charts';
+import { Component } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent {
+  title = 'ng2-charts-demo';
+
   // Doughnut
-  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData: MultiDataSet = [
-    [350, 450, 100],
-    [50, 150, 120],
-    [250, 130, 70],
-  ];
-  public doughnutChartType: ChartType = 'doughnut';
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] = [
+      { data: [ 350, 450, 100 ], label: 'Series A' },
+      { data: [ 50, 150, 120 ], label: 'Series B' },
+      { data: [ 250, 130, 70 ], label: 'Series C' }
+    ];
 
-  constructor() { }
+  public doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
+    responsive: false
+  };
 
-  ngOnInit() {
+  constructor() {
   }
+
 }
